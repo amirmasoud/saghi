fff16782ef5f0612555c4fcfb7c1cc1af3bf4267
Amirmasoud <amirmasood33@gmail.com>
2023-05-10 06:16:14 -0600

    Home page

---
+++ resources/views/parts/header.blade.php
@@ -0,0 +1,26 @@
+<header class="bg-white">
+    <nav class="mx-auto max-w-7xl px-6">
+        <div class="flex w-full items-center justify-between py-2">
+            <div class="flex items-center">
+                <a href="#">
+                    <span class="sr-only">ساقی</span>
+                    <img class="h-10 w-auto" src="#" alt="">
+                </a>
+                <div class="mr-10 hidden space-x-8 space-x-reverse lg:block">
+                    <a href="#" class="text-base text-sm font-medium text-black hover:text-gray-700">خانه</a>
+                    <a href="#" class="text-base text-sm font-medium text-black hover:text-gray-700">شاعران</a>
+                    <a href="#" class="text-base text-sm font-medium text-black hover:text-gray-700">کتاب‌ها</a>
+                </div>
+            </div>
+            <div class="mr-10 space-x-4 space-x-reverse">
+                <a href="#" class="inline-block rounded-md border border-transparent bg-white py-2 px-4 text-base text-sm font-medium text-black hover:bg-opacity-75">ورود</a>
+                <a href="#" class="inline-block rounded-md border border-transparent bg-black py-2 px-4 text-base text-sm font-medium text-white hover:bg-gray-700">ثبت نام</a>
+            </div>
+        </div>
+        <div class="flex flex-wrap justify-center gap-x-6 py-4 lg:hidden">
+            <a href="#" class="text-base text-sm font-medium text-black hover:text-gray-700">خانه</a>
+            <a href="#" class="text-base text-sm font-medium text-black hover:text-gray-700">شاعران</a>
+            <a href="#" class="text-base text-sm font-medium text-black hover:text-gray-700">کتاب‌ها</a>
+        </div>
+    </nav>
+</header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,26 @@
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         @livewireStyles
+        @vite('resources/css/app.css')
     </head>
     <body dir="rtl">
-        <ul>
-            @foreach($authors as $author)
-                <li><a href="{{ $author->slug }}">{{ $author->name }}</a></li>
-            @endforeach
-        </ul>
+        @include('parts.header')
+        <div class="mx-auto max-w-2xl px-6 lg:max-w-7xl py-4">
+
+            <div class="grid grid-cols-1 gap-y-12 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                @foreach($authors as $author)
+                <a href="/{{ $author->slug }}" class="group">
+                    <div class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gradient-to-b from-gray-700 to-gray-900 xl:aspect-w-7 xl:aspect-h-8">
+                        <img src="https://picsum.photos/1280/1280?random={{ $loop->index }}" class="h-full w-full object-cover object-center group-hover:opacity-80">
+                    </div>
+                    <p class="mt-2 text-lg font-semibold text-gray-900">{{ $author->name }}</p>
+                </a>
+                @endforeach
+            </div>
+
+        </div>
         @livewireScripts
     </body>
 </html>
