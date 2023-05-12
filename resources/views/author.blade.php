032eb0ed115af344c9ab282fcba7a36fe9fde753
Amirmasoud <amirmasood33@gmail.com>
2023-05-12 06:58:08 -0600

    author page

--- resources/views/author.blade.php
+++ resources/views/author.blade.php
@@ -1,15 +1,27 @@
-<html>
-<head>
-    @livewireStyles
-</head>
-<body dir="rtl">
-<span><a href="/">Home</a></span>
-<h1>{{ $author->name }}</h1>
-<ul>
-    @foreach($books as $book)
-        <li><a href="{{ $author->slug }}/{{ $book->slug }}">{{ $book->title }}</a></li>
-    @endforeach
-</ul>
-@livewireScripts
-</body>
-</html>
+@extends('app')
+
+@section('content')
+    <div class="md:flex md:items-center md:justify-between md:space-x-5 md:space-x-reverse">
+        <div class="flex items-start space-x-5 space-x-reverse">
+            <div class="flex-shrink-0">
+                <div class="relative">
+                    <img class="h-16 w-16 rounded-lg" src="https://picsum.photos/1280/1280" alt="">
+                    <span class="absolute inset-0 rounded-lg shadow-inner" aria-hidden="true"></span>
+                </div>
+            </div>
+            <div class="pt-1.5">
+                <h1 class="text-2xl font-bold text-gray-900">{{ $author->name }}</h1>
+                <p class="text-sm font-medium text-gray-500">{{ $author->birth_year_in_hijri ?? 'نامعلوم' }} &mdash; {{ $author->death_year_in_hijri ?? 'نامعلوم' }}</p>
+            </div>
+        </div>
+        <div class="justify-stretch mt-6 flex flex-col-reverse space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-start sm:space-y-0 sm:space-x-3 md:mt-0 md:flex-row md:space-x-3 md:space-x-reverse">
+            <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-gray-900 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 focus:ring-offset-gray-100">دنبال کردن</button>
+        </div>
+    </div>
+
+    <ul>
+        @foreach($books as $book)
+            <li><a href="{{ $author->slug }}/{{ $book->slug }}">{{ $book->title }}</a></li>
+        @endforeach
+    </ul>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,14 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        @livewireStyles
-        @vite('resources/css/app.css')
-    </head>
-    <body dir="rtl">
-        @include('parts.header')
-        <div class="mx-auto max-w-2xl px-6 lg:max-w-7xl py-4">
+@extends('app')
 
-            <div class="grid grid-cols-1 gap-y-12 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                @foreach($authors as $author)
-                <a href="/{{ $author->slug }}" class="group">
-                    <div class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gradient-to-b from-gray-700 to-gray-900 xl:aspect-w-7 xl:aspect-h-8">
-                        <img src="https://picsum.photos/1280/1280?random={{ $loop->index }}" class="h-full w-full object-cover object-center group-hover:opacity-80">
-                    </div>
-                    <p class="mt-2 text-lg font-semibold text-gray-900">{{ $author->name }}</p>
-                </a>
-                @endforeach
+@section('content')
+<div class="grid grid-cols-2 gap-y-12 gap-x-8 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 xl:gap-x-8">
+    @foreach($authors as $author)
+        <a href="/{{ $author->slug }}" class="group">
+            <div class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gradient-to-b from-gray-700 to-gray-900">
+                <img src="https://picsum.photos/1280/1280?random={{ $loop->index }}" class="h-full w-full object-cover object-center group-hover:opacity-80">
             </div>
-        </div>
-        @include('parts.socials')
-        @livewireScripts
-    </body>
-</html>
+            <p class="mt-2 text-lg font-semibold text-gray-900">{{ $author->name }}</p>
+        </a>
+    @endforeach
+</div>
+@endsection
