0b0d7cf21ab4c3f3a4efad566b36c562730c5eb7
Amirmasoud <amirmasood33@gmail.com>
2023-07-04 18:35:31 -0600

    Testing Statamic

--- routes/web.php
+++ routes/web.php
@@ -14,35 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome', [
-        'authors' => \App\Models\Author::all(),
-    ]);
+    return view('welcome');
 });
-
-Route::get('/{author:slug}', function (\App\Models\Author $author) {
-    return view('author', [
-        'author' => $author,
-        'books' => $author->books,
-    ]);
-});
-
-Route::get('/{author:slug}/{book:slug}', function (\App\Models\Author $author, \App\Models\Book $book) {
-    return view('book', [
-        'author' => $author,
-        'book' => $book,
-        'books' => $author->books,
-        'pages' => $book->pages,
-    ]);
-});
-
-Route::get('/{author:slug}/{book:slug}/{page:id}', function (\App\Models\Author $author, \App\Models\Book $book, \App\Models\Page $page) {
-    return view('page', [
-        'author' => $author,
-        'book' => $book,
-        'books' => $author->books,
-        'pages' => $book->pages,
-        'content' => $page,
-        'next' => $book->pages()->where('id', '>', $page->id)->first(),
-        'previous' => $book->pages()->where('id', '<', $page->id)->orderBy('order', 'desc')->first(),
-    ]);
-})->where('page', '(.*)');
