dcc8ecbbf63f8d046d9d95f0b3d741d9e364afdb
Amirmasoud <amirmasood33@gmail.com>
2024-02-24 16:00:33 -0700

    wip

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,21 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->text('name');
+            $table->text('slug');
+            $table->text('nickname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('published_at')->nullable();
+            $table->integer('birth_year_in_hijri')->nullable();
+            $table->boolean('birth_year_validated_at')->nullable();
+            $table->text('birth_place')->nullable();
+            $table->string('birth_place_location')->nullable();
+            $table->integer('death_year_in_hijri')->nullable();
+            $table->integer('death_year_validated_at')->nullable();
+            $table->text('death_place')->nullable();
+            $table->string('death_place_location')->nullable();
+            $table->string('order_column')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
